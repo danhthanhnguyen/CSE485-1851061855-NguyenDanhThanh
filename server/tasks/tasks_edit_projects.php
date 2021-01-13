05101829a51b00e7f995b556158c1381ec014730
thanhnguyen <nguyendanhthanh980@gmail.com>
2021-01-13 13:38:08 +0700

    process edit projects feature

---
+++ server/tasks/tasks_edit_projects.php
@@ -0,0 +1,18 @@
+<?php
+  require("../database/dbfunction.php");
+  if(isset($_POST["editProjects"])) {
+    $getIdProjects = $_REQUEST["id"];
+    if($_REQUEST["thumbnail"] != "") {
+      $thumbnail = $_REQUEST["thumbnail"];
+      queryManipulation("UPDATE projects SET thumbnail=? WHERE id_projects=?", "set", [$thumbnail, $getIdProjects]);
+    }
+    //update
+    $name = $_REQUEST["name"];
+    $platform = $_REQUEST["platform"];
+    $link = $_REQUEST["link"];
+    $description = $_REQUEST["description"];
+    $createAt = $_REQUEST["create"];
+    echo $name.$platform.$link.$description.$createAt;
+    queryManipulation("UPDATE projects SET name=?, platform=?, link=?, description=?, create_at=? WHERE id_projects=?", "set", [$name, $platform, $link, $description, $createAt, $getIdProjects]);
+  }
+?>
