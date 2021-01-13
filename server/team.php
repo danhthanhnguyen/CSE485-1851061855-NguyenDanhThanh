58abfb3d02718a3827b4c6e9fb12462841ed046c
thanhnguyen <nguyendanhthanh980@gmail.com>
2021-01-13 13:15:50 +0700

    add feature for team.php

--- server/team.php
+++ server/team.php
@@ -46,7 +46,7 @@
               <h3>
                 My Team
                 <a
-                  href="roles.html"
+                  href="<?php echo constant("URL")."/server/update/add_team.php"; ?>"
                   class="btn btn-sm btn-outline-primary float-right"
                   ><i class="fas fa-plus-circle"></i> Add</a
                 >
