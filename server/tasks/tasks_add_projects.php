073a55a819c62300dc400edd192d863846836f42
thanhnguyen <nguyendanhthanh980@gmail.com>
2021-01-15 22:47:09 +0700

    Fix: project record conflict

--- server/tasks/tasks_add_projects.php
+++ server/tasks/tasks_add_projects.php
@@ -7,7 +7,14 @@
     $link = $_REQUEST["link"];
     $platform = $_REQUEST["platform"];
     $createAt = $_REQUEST["create"];
-    queryManipulation("INSERT INTO projects(name, thumbnail, platform, link, description, create_at) VALUES(?, ?, ?, ?, ?, ?)", "set", [$name, $thumbnail, $platform, $link, $description, $createAt]);
-    echo "Done!";
+    //check record confict
+    $checkProject = queryManipulation("SELECT name FROM projects WHERE name=?", "get", [$name]);
+    if(count($checkProject) > 0) {
+      echo "This project already exists";
+      return;
+    } else {
+      queryManipulation("INSERT INTO projects(name, thumbnail, platform, link, description, create_at) VALUES(?, ?, ?, ?, ?, ?)", "set", [$name, $thumbnail, $platform, $link, $description, $createAt]);
+      echo "Done!";
+    }
   }
 ?>
