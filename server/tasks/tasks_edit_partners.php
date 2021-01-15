6dd5368dc21746bc478032fa24b1fc680565c20a
thanhnguyen <nguyendanhthanh980@gmail.com>
2021-01-15 22:49:01 +0700

    Fix: edit something

--- server/tasks/tasks_edit_partners.php
+++ server/tasks/tasks_edit_partners.php
@@ -7,13 +7,13 @@
       queryManipulation("UPDATE partners SET logo=? WHERE id_partners=?", "set", [$logo, $getIdPartners]);
     }
     //update
-    $comapny = $_REQUEST["company"];
+    $company = $_REQUEST["company"];
     $email = $_REQUEST["email"];
     $link = $_REQUEST["link"];
     $description = $_REQUEST["description"];
     $headquarter = $_REQUEST["headquarter"];
     $field = $_REQUEST["field"];
     $createAt = $_REQUEST["create"];
-    queryManipulation("UPDATE partners SET company=?, email=?, link=?, description=?, headquarter=?, field=?, create_at=? WHERE id_partners=?", "set", [$comapny, $email, $link, $description, $headquarter, $field, $createAt, $getIdPartners]);
+    queryManipulation("UPDATE partners SET company=?, email=?, link=?, description=?, headquarter=?, field=?, create_at=? WHERE id_partners=?", "set", [$company, $email, $link, $description, $headquarter, $field, $createAt, $getIdPartners]);
   }
 ?>
