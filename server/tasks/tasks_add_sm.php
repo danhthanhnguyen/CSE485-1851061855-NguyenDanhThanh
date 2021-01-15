961b778ce9240a08ee0539fc0043e62b8012ad0d
thanhnguyen <nguyendanhthanh980@gmail.com>
2021-01-15 22:47:50 +0700

    Fix: social media record conflict

--- server/tasks/tasks_add_sm.php
+++ server/tasks/tasks_add_sm.php
@@ -4,7 +4,14 @@
     $name = $_REQUEST["name"];
     $link = $_REQUEST["link"];
     $icon = $_REQUEST["icon"];
-    queryManipulation("INSERT INTO social_media(name, link, icon) VALUES(?, ?, ?)", "set", [$name, $link, $icon]);
-    echo "Done!";
+    //check record conflict
+    $checkSm = queryManipulation("SELECT name FROM social_media WHERE name=?", "get", [$name]);
+    if(count($checkSm) > 0) {
+      echo "This social media already exists";
+      return;
+    } else {
+      queryManipulation("INSERT INTO social_media(name, link, icon) VALUES(?, ?, ?)", "set", [$name, $link, $icon]);
+      echo "Done!";
+    }
   }
 ?>
