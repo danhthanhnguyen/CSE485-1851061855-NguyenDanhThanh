02d0b9b8dde608a67a787176eb1961b20c69c89b
thanhnguyen <nguyendanhthanh980@gmail.com>
2021-01-15 22:47:29 +0700

    Fix: skill record conflict

--- server/tasks/tasks_add_skills.php
+++ server/tasks/tasks_add_skills.php
@@ -4,7 +4,14 @@
     $name = $_REQUEST["name"];
     $type = $_REQUEST["type"];
     $icon = $_REQUEST["icon"];
-    queryManipulation("INSERT INTO skills(name, type, icon) VALUES(?, ?, ?)", "set", [$name, $type, $icon]);
-    echo "Done!";
+    //check record conflict
+    $checkSkill = queryManipulation("SELECT name FROM skills WHERE name=?", "get", [$name]);
+    if(count($checkSkill) > 0) {
+      echo "This skill already exists";
+      return;
+    } else {
+      queryManipulation("INSERT INTO skills(name, type, icon) VALUES(?, ?, ?)", "set", [$name, $type, $icon]);
+      echo "Done!";
+    }
   }
 ?>
